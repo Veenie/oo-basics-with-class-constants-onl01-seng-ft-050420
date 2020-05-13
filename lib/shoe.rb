9bed4f5e771a4a3fc75bfaed8095c6ec9848bc85
Raveen K Sharma <raveen08@gmail.com>
2020-05-13 20:59:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,12 @@ class Shoe
 
   BRANDS = []
   
- 
+   def initialize(brand)
+    @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
+  end
 
   def cobble
     self.condition = "new"
